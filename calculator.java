ff88906b9e8001b569aac7c8074e28ebda7e9a80
nitinsreevstv <76206822+nitinsreevstv@users.noreply.github.com>
2024-05-13 23:08:45 +0530

    code String[] args

--- calculator.java
+++ calculator.java
@@ -3,7 +3,7 @@ package javaPractice;
 import java.util.Scanner;
 
 public class calculator{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter First Number");
         int first = sc.nextInt();
