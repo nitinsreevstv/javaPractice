0ead4072f6e675b0a8c3ceccc30b317b5927d841
nitinsreevstv <76206822+nitinsreevstv@users.noreply.github.com>
2024-05-13 23:05:19 +0530

    calculator code

---
+++ calculator.java
@@ -0,0 +1,35 @@
+package javaPractice;
+
+import java.util.Scanner;
+
+public class calculator{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int first = sc.nextInt();
+        System.out.println("Enter First Number");
+        int second = sc.nextInt();
+        System.out.println("Enter operation to perform (+,-,*,/,%)");
+        char ch = sc.next().charAt(0);
+        switch (ch) {
+            case '+':
+                System.out.println(first + second);
+                break;
+            case '-':
+                System.out.println(first - second);
+                break;
+            case '*':
+                System.out.println(first * second);
+                break;
+            case '/':
+                System.out.println(first / second);
+                break;
+            case '%':
+                System.out.println(first % second);
+                break;
+            default:
+                System.out.println("Please provide valid operator");
+                break;
+        }
+    }
+}
