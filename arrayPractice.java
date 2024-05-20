08f8baedc9205434290e780f885e3fd18103c78c
nitinsreevstv <76206822+nitinsreevstv@users.noreply.github.com>
2024-05-21 01:39:06 +0530

    ArrayPractice

---
+++ arrayPractice.java
@@ -0,0 +1,47 @@
+package javaPractice;
+
+import java.util.*;
+
+public class arrayPractice{
+    public static void reverse(int arr[]){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static int largest(int arr[]){
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < arr.length; i++){
+            if(max< arr[i]){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String args[]){
+        System.out.print("Enter the length of array");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter the Number for "+ (i + 1) + " index");
+            arr[i] = sc.nextInt();
+        }
+        
+        printArr(arr);
+        reverse(arr);
+        printArr(arr);
+        System.out.println("The largest number in this array is " + largest(arr));
+    }
+}
