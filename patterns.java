b69e3c884e5320155d3276d026025aa6a98955aa
nitinsreevstv <76206822+nitinsreevstv@users.noreply.github.com>
2024-05-20 14:24:36 +0530

    "pattens types added"

--- patterns.java
+++ patterns.java
@@ -6,7 +6,7 @@ public class patterns{
     public static void pattern1(int n){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j<= i; j++){
-                System.out.print("*");
+                System.out.print("*" + " ");
             }
             System.out.println();
         }
@@ -15,9 +15,9 @@ public class patterns{
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= i; j++){
                 if((i+j)%2==0){
-                    System.out.print("1");
+                    System.out.print("1" + " ");
                 }else{
-                    System.out.print("0");
+                    System.out.print("0" + " ");
                 }
             }
             System.out.println();
@@ -26,6 +26,35 @@ public class patterns{
     public static void space(int n){
         for(int i = 0; i < n; i++){
             System.out.println("----------------");
+        }
+    }
+    public static void revPattern(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern3(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i + 1; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void floydPattern(int n){
+        int dig = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(dig + " ");
+                dig++;
+            }
+            System.out.println();
         }
     }
     public static void main(String[] args){
@@ -37,6 +66,11 @@ public class patterns{
         space(2);
         pattern2(n);
         space(2);
+        revPattern(n);
+        space(2);
+        pattern3(n);
+        space(2);
+        floydPattern(n);
+        space(2);
     }
-
 }
